5c770447498ece249773c759e2c38bd2e0485ae3
WaterLiliths <waterliliths@gmail.com>
2026-08-27 20:48:11 -0300

    Clase Animation implementada
    Se implementó la clase Animation
    - Se agregó función para la carga de animaciones
    - Se agregó función para el update de animaciones
    - Se agregó función para el renderizado de animaciones
    
    Se subio cat_spritesheet a la carpeta assets

---
+++ classes/animations.lua
@@ -0,0 +1,66 @@
+-- ================ CLASE ===============
+local Animation = {}
+Animation.__index = Animation
+
+-- =============== INICIALIZACION ==================
+function Animation:new(img, frames_count, width, height, speed, is_vertical)
+
+    local animation = setmetatable({}, Animation)
+
+    animation.width = width
+    animation.height = height
+    animation.origin_x = width / 2
+    animation.origin_y = height / 2
+    animation.spritesheet = love.graphics.newImage(img)
+    animation.iterator = 1
+    animation.speed = speed
+    animation.quads = {}
+    animation.is_active = true
+
+    if is_vertical then
+        -- (frames_count - 1) por claridad al calcular la cantidad de frames a cargar
+        for i = 0, frames_count - 1, 1 do
+           table.insert(animation.quads,
+        love.graphics.newQuad(0, animation.height * i, animation.width, animation.height, 
+        animation.spritesheet)
+        ) 
+        end
+    else
+        for i = 0, frames_count -1, 1 do
+          table.insert(animation.quads,
+        love.graphics.newQuad(animation.width * i, 0, animation.width, animation.height,
+        animation.spritesheet)
+        )  
+        end
+    end
+
+    return animation
+end
+
+-- ================ UPDATE ======================
+
+function Animation:update(dt)
+
+    if not self.is_active then --condicion de corte
+        return
+    end
+
+    self.iterator = self.iterator + (self.speed * dt)
+    if self.iterator >= #self.quads + 1 then
+        self.iterator = 1
+    end
+end
+
+-- ================ DIBUJADO ===================
+
+function Animation:render (x, y)
+    local current_frame = math.floor(self.iterator) -- transforma el iterator a un entero
+    
+    love.graphics.draw(self.spritesheet,
+        self.quads[current_frame],
+        x, y, 0, 1, 1,
+        self.origin_x, self.origin_y
+        )
+end
+
+return Animation
