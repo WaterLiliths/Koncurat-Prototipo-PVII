8855d1122eee3cdf7547e2b910bb6ab45362efb8
WaterLiliths <waterliliths@gmail.com>
2026-08-31 14:22:58 -0300

    Canvas creado
    Se creó el canvas para agrandar los sprites

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 local Player = require("classes.player")
 
 local player
+local canvas
 
 function love.load ()
-    player = Player:new(200, 200)
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    canvas = love.graphics.newCanvas(640, 360)
+    player = Player:new(160, 90)
 end
 
 function love.update (dt)
@@ -11,5 +14,9 @@ function love.update (dt)
 end
 
 function love.draw ()
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear()
     player:draw()
+    love.graphics.setCanvas()
+    love.graphics.draw(canvas, 0, 0, 0, 2, 2)
 end
